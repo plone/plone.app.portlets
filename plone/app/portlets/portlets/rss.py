4f7a9b0133a9cf04628d3637feec8b3b0da86520
Wichert Akkerman <wichert@wiggy.net>
2008-06-18 16:43:03 +0000

    Add support for system default proxies
    svn path=/plone.app.portlets/branches/plip225-proxy/; revision=21204

--- plone/app/portlets/portlets/rss.py
+++ plone/app/portlets/portlets/rss.py
@@ -19,11 +19,15 @@ from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 import feedparser
 import time, socket
 from urllib2 import ProxyHandler
+from urllib2 import getproxies
 
 from DateTime import DateTime
 
 # store the feeds here (which means in RAM)
 FEED_DATA = {}  # url: ({date, title, url, itemlist})
+
+# Default proxies
+DEFAULT_PROXIES = getproxies()
 
 class IFeed(Interface):
     
@@ -253,10 +257,17 @@ class Renderer(base.DeferredRenderer):
         if feed is None:
             # create it
             handlers = []
-            html_proxy = getToolByName(self.context, 'portal_properties').site_properties.getProperty('html_proxy', None)
+            sp = getToolByName(self.context, 'portal_properties').site_properties
+            html_proxy = sp.getProperty('html_proxy', None)
             if html_proxy is not None:
-                handlers.append(ProxyHandler({'http':html_proxy}))
-            feed = FEED_DATA[self.data.url] = RSSFeed(self.data.url,self.data.timeout,handlers=handlers)
+                proxies = DEFAULT_PROXIES.copy()
+                proxies["html"]=html_proxy
+            else:
+                proxies = DEFAULT_PROXIES
+            if proxies:
+                handlers.append(ProxyHandler(proxies))
+            feed = FEED_DATA[self.data.url] = RSSFeed(self.data.url,
+                    self.data.timeout, handlers=handlers)
         return feed
         
     @property
