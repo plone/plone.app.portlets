4ed7b226ee980cd5604bdc4b0aeb4a587713e729
Ramon Navarro Bosch <ramon.nb@gmail.com>
2014-02-11 10:00:28 +0100

    Merging toolbar porltet manager on portlets

--- plone/app/portlets/browser/manage.py
+++ plone/app/portlets/browser/manage.py
@@ -363,3 +363,22 @@ class ManageContentTypePortletsViewlet(ManagePortletsViewlet):
     manage content type portlets screen.
     """
     implements(IManageContentTypePortletsView)
+
+
+class TopbarManagePortlets(ManageContextualPortlets):
+
+    def __init__(self, context, request):
+        super(TopbarManagePortlets, self).__init__(context, request)
+        # Disable the left and right columns
+        self.request.set('disable_plone.leftcolumn', 1)
+        self.request.set('disable_plone.rightcolumn', 1)
+        # Initialize the manager name in case there is nothing
+        # in the traversal path
+        self.manager_name = 'plone.leftcolumn'
+
+    def publishTraverse(self, request, name):
+        """Get the portlet manager via traversal so that we can re-use
+        the portlet machinery without overriding it all here.
+        """
+        self.manager_name = name
+        return self
