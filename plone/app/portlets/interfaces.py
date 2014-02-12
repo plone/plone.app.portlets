5b999f9b700c7054fab9c0a3e7253ac7ed9c6774
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2014-02-12 10:16:31 +0100

    add the interface for the new footer portletmanager

--- plone/app/portlets/interfaces.py
+++ plone/app/portlets/interfaces.py
@@ -60,6 +60,11 @@ class IRightColumn(IColumn):
     Normally, you will register portlets for IColumn instead.
     """
 
+class IFooter(IColumn):
+    """The footer
+
+    Normally, you will register portlets for IColumn instead.
+    """
 
 class IDashboard(IDefaultPortletManager, IPlacelessPortletManager):
     """Common base class for dashboard columns
