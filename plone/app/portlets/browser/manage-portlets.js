49c125155bb23601e6a464de487402c018cb1d9c
vangheem <vangheem@gmail.com>
2015-05-14 00:31:30 -0500

    edit in modal also

--- plone/app/portlets/browser/manage-portlets.js
+++ plone/app/portlets/browser/manage-portlets.js
@@ -16,8 +16,6 @@ define([
     dirty: false,
     init: function(){
       var that = this;
-      that.setupAddDropdown();
-      that.setupSavePortletsSettings();
       var $modal = that.$el.parents('.plone-modal');
       if($modal.size() === 1){
         this.isModal = true;
@@ -29,11 +27,18 @@ define([
           }
         });
       }
+      that.bind();
+    },
+    bind: function(){
+      var that = this;
+      that.setupAddDropdown();
+      that.setupSavePortletsSettings();
+      that.setupPortletEdit();
     },
     rebind: function($el){
       this.$el.replaceWith($el);
       this.$el = $el;
-      this.init();
+      this.bind();
       this.statusMessage();
       this.dirty = true;
     },
@@ -61,6 +66,49 @@ define([
         $message.fadeOut();
       }, 3000);
     },
+    showEditPortlet: function(url){
+      var that = this;
+      var $a = $('<a/>');
+      $('body').append($a);
+      var pattern = new Modal($a, {
+        ajaxUrl: url,
+        actionOptions: {
+          displayInModal: false,
+          reloadWindowOnClose: false,
+          isForm: true,
+          onSuccess: function(modal, html){
+            pattern.hide();
+            var $body = $(utils.parseBodyTag(html));
+            that.rebind($('#' + that.$el.attr('id'), $body));
+            that.statusMessage(_t('Portlet added'));
+          }
+        }
+      });
+      pattern.on('after-render', function(){
+        var $el = $('#' + that.$el.attr('id'), pattern.$raw);
+        /* this is a check that the add form doesn't just automatically
+           create the portlet without an actual form.
+           If element is found here, we can short circuit and
+           continue on. */
+        if($el.size() === 1){
+          /* hacky, trying to prevent modal parts from flickering here */
+          $el = $el.clone();
+          pattern.on('shown', function(){
+            pattern.hide();
+          });
+          that.rebind($el);
+          that.statusMessage(_t('Portlet added'));
+        }
+      });
+      pattern.show();
+    },
+    setupPortletEdit: function(){
+      var that = this;
+      $('.managedPortlet .portletHeader > a', that.$el).click(function(e){
+        e.preventDefault();
+        that.showEditPortlet($(this).attr('href'));
+      });
+    },
     setupAddDropdown: function(){
       var that = this;
       $('.add-portlet', that.$el).change(function(e){
@@ -71,40 +119,7 @@ define([
         var url = contextUrl + $select.val() +
           '?_authenticator=' + $('[name="_authenticator"]').val() +
           '&referer=' + $('[name="referer"]', $form).val();
-        /* create model */
-        var $a = $('<a/>');
-        $('body').append($a);
-        var pattern = new Modal($a, {
-          ajaxUrl: url,
-          actionOptions: {
-            displayInModal: false,
-            reloadWindowOnClose: false,
-            isForm: true,
-            onSuccess: function(modal, html){
-              pattern.hide();
-              var $body = $(utils.parseBodyTag(html));
-              that.rebind($('#' + that.$el.attr('id'), $body));
-              that.statusMessage(_t('Portlet added'));
-            }
-          }
-        });
-        pattern.on('after-render', function(){
-          var $el = $('#' + that.$el.attr('id'), pattern.$raw);
-          /* this is a check that the add form doesn't just automatically
-             create the portlet without an actual form.
-             If element is found here, we can short circuit and
-             continue on. */
-          if($el.size() === 1){
-            /* hacky, trying to prevent modal parts from flickering here */
-            $el = $el.clone();
-            pattern.on('shown', function(){
-              pattern.hide();
-            });
-            that.rebind($el);
-            that.statusMessage(_t('Portlet added'));
-          }
-        });
-        pattern.show();
+        that.showEditPortlet(url);
       });
     },
     setupSavePortletsSettings: function(){
