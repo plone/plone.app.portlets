25b7e732e5ff974f9d311cf92c7d73c70cbc9743
esteele <eric@esteele.net>
2015-09-17 17:40:07 +0200

    Be defensive in lookups so that unmigrated sites aren't broken.

--- plone/app/portlets/portlets/navigation.py
+++ plone/app/portlets/portlets/navigation.py
@@ -366,13 +366,9 @@ class QueryBuilder(object):
 
         # Filter on workflow states, if enabled
         registry = getUtility(IRegistry)
-        navigation_settings = registry.forInterface(
-            INavigationSchema,
-            prefix="plone"
-        )
-        if navigation_settings.filter_on_workflow:
-            query['review_state'] = navigation_settings.workflow_states_to_show
-
+        if registry.get('plone.filter_on_workflow', False):
+            query['review_state'] = registry.get(
+                'plone.workflow_states_to_show', ())
         self.query = query
 
     def __call__(self):
