ccf256132e2ca62b4020aa0a41fee5d89da081ce
esteele <eric@esteele.net>
2015-09-18 12:04:17 +0200

    Fix test to use registry for 'root' setting

--- plone/app/portlets/tests/test_navigation_portlet.py
+++ plone/app/portlets/tests/test_navigation_portlet.py
@@ -357,8 +357,8 @@ class TestRenderer(PortletsTestCase):
         self.assertEqual(len(tree['children']), 6)
 
     def testAboveRoot(self):
-        ntp=self.portal.portal_properties.navtree_properties
-        ntp.manage_changeProperties(root='/folder2')
+        registry = getUtility(IRegistry)
+        registry['plone.root'] = u'/folder2'
         view = self.renderer(self.portal)
         tree = view.getNavTree()
         self.assertTrue(tree)
