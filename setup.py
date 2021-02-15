4bd2c19fa664a7bbf56c469c3f18406528ff509b
Peter Mathis <peter.mathis@kombinat.at>
2021-02-15 15:40:46 +0100

    update classifiers

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '4.4.7.dev0'
+version = '5.0.0.dev0'
 
 setup(
     name='plone.app.portlets',
@@ -13,16 +13,11 @@ setup(
     open("CHANGES.rst").read(),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone :: 5.2",
+        "Framework :: Plone :: 6.0",
         "Framework :: Plone :: Core",
-        "Framework :: Zope2",
-        "Framework :: Zope :: 4",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-        "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
