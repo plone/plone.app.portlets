39ec57e6847b5d54874a37cacf7c790ac1fe9d19
Maurits van Rees <maurits@vanrees.org>
2021-06-30 21:51:41 +0200

    Add classifier Py 3.9.
    [ci skip]

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords='portlets viewlets plone',
     author='Plone Foundation',
