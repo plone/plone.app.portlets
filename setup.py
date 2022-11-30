04f95a1967b8010050148282b8c479e755e09450
Gil Forcada Codinachs <gil.gnome@gmail.com>
2022-11-30 07:21:36 +0100

    chore: specify that python 3.8 as minimum version

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     namespace_packages=["plone", "plone.app"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     extras_require=dict(
         test=[
             "plone.app.testing",
