1f033a291387eac29068944b4dbee1773f6b3565
Maurits van Rees <maurits@vanrees.org>
2023-03-29 19:27:14 +0200

    Removed unused definition of HAS_PLONE_APP_EVENT.
    plone.app.event is always there.

--- plone/app/portlets/__init__.py
+++ plone/app/portlets/__init__.py
@@ -1,14 +1,5 @@
 # Import PloneMessageFactory to create messages in the plone domain
 from zope.i18nmessageid import MessageFactory
 
-import pkg_resources
-
 
 PloneMessageFactory = MessageFactory("plone")
-
-try:
-    pkg_resources.get_distribution("plone.app.event")
-except pkg_resources.DistributionNotFound:
-    HAS_PLONE_APP_EVENT = False
-else:
-    HAS_PLONE_APP_EVENT = True
