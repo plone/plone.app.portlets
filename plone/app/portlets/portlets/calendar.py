ef683ac34672bfb43e2a345075c328128510df13
Jens W. Klein <jk@kleinundpartner.at>
2023-04-24 23:02:53 +0200

    fix circular dependency on plone.app.event for bbb code

--- plone/app/portlets/portlets/calendar.py
+++ plone/app/portlets/portlets/calendar.py
@@ -1,2 +1,7 @@
-# avoid broken persistent object
-from plone.app.event.portlets.portlet_calendar import Assignment  # noqa F401
+from zope.deferredimport import deprecated
+
+
+deprecated(
+    "Import from plone.app.event.portlets.portlet_calendar instead (will be removed in Plone 7)",
+    Assignment="plone.app.event.portlets.portlet_calendar:Assignment",
+)
